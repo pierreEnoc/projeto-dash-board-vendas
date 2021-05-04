5171b0cc61d27fe426ce8b11c5e50ae4c2e32998
Pierre enoc <pierre.enoc@gympass.com>
2021-05-03 22:50:38 -0300

    basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
+import Datatable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary"> Ola</h1>
+    <>
+    <NavBar />
+    <div className="container"> 
+      <h1 className="text-primary">Olá mundo!</h1>
+
+    <Datatable />
     </div>
+    <Footer />
+   </>
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">App desenvolvido por <a href="https://github.com/pierreEnoc" target="_blank" rel="noreferrer">pierre Enoc</a></p>
+          <p className="text-light"><small><strong>Vendas</strong><br/>
+            Dash board de vendas: <a href="https://www.linkedin.com/in/pierre-enoc-84309b5b/" target="_blank" rel="noreferrer">@pierre</a></small></p>
+        </div>
+      </footer>
+    );
+  }
+  
+  export default Footer;

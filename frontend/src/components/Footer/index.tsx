b21507dc2ea32bb782d82b4a2098e7a3daca0868
Pierre enoc <pierre.enoc@gympass.com>
2021-05-12 09:40:47 -0300

    refactor

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <footer className="footer mt-auto py-3 bg-dark">
         <div className="container">
           <p className="text-light">App desenvolvido por <a href="https://github.com/pierreEnoc" target="_blank" rel="noreferrer">pierre Enoc</a></p>
-          <p className="text-light"><small><strong>Vendas</strong><br/>
+          <p className="text-light"><small><strong></strong>
             Dash board de vendas: <a href="https://www.linkedin.com/in/pierre-enoc-84309b5b/" target="_blank" rel="noreferrer">@pierre</a></small></p>
         </div>
       </footer>
